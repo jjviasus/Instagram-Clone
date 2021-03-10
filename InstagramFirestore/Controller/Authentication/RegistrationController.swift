8951c425b0dbc7367b88bc378f38cb2a7de3b878
jjviasus <61569318+jjviasus@users.noreply.github.com>
2021-03-10 00:30:13 -0500

    login page

---
+++ InstagramFirestore/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestore
+//
+//  Created by Justin Viasus on 3/9/21.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
