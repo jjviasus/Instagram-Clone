c7e563792e1e0fb972e28ab335aa1ddd8b5ef94b
jjviasus <61569318+jjviasus@users.noreply.github.com>
2021-04-06 17:57:21 -0400

    Fetching & Displaying Users

--- InstagramFirestore/API/UserService.swift
+++ InstagramFirestore/API/UserService.swift
@@ -23,4 +23,20 @@ struct UserService {
             completion(user)
         }
     }
+    
+    static func fetchUsers(completion: @escaping([User]) -> Void) { // a completion block that gives us back an array of User's
+        
+        COLLECTION_USERS.getDocuments { (snapshot, error) in
+            guard let snapshot = snapshot else { return }
+            
+            // a mapping function takes one data object and then performs some sort of mapping
+            // and converts it into another data object
+            
+            // looks at the documents array from the snapshot.
+            // we want to map the documents into a user object, $0 represents each one of the documents data in the document array
+            let users = snapshot.documents.map({ User(dictionary: $0.data()) })
+            completion(users)
+        }
+        
+    }
 }
--- InstagramFirestore/Controller/SearchController.swift
+++ InstagramFirestore/Controller/SearchController.swift
@@ -7,11 +7,56 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    private var users = [User]()
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemPink
+        configureTableView()
+        fetchUsers()
+    }
+    
+    // MARK: - API
+    
+    func fetchUsers() {
+        UserService.fetchUsers { users in
+            self.users = users
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    func configureTableView() {
+        view.backgroundColor = .white
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier) // here we are registering a cell that has a reuse identifier
+        tableView.rowHeight = 64
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchController {
+    // defines how many rows the table view has
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    // defines what each cell in the table view contains
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        //print("Func called")
+        //print("DEBUG: Index path row is \(indexPath.row)")
+        cell.user = users[indexPath.row]
+        return cell
     }
 }
