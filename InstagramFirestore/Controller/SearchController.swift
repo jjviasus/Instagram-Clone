02f9606fe3e14ac2d0ed7d8240d2d7180c57b5b7
jjviasus <61569318+jjviasus@users.noreply.github.com>
2021-04-12 17:45:39 -0400

    Searching For Users

--- InstagramFirestore/Controller/SearchController.swift
+++ InstagramFirestore/Controller/SearchController.swift
@@ -14,12 +14,21 @@ class SearchController: UITableViewController {
     // MARK: - Properties
     
     private var users = [User]()
+    private var filteredUsers = [User]()
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    // Determines whether or not the user is searching something.
+    // If the search controller is active this means they clicked inside the search bar.
+    // We check for not empty because we only want to be in search mode if the search controller is active and the user is typing (non-empty).
+    private var inSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureSearchController()
         configureTableView()
         fetchUsers()
     }
@@ -41,6 +50,15 @@ class SearchController: UITableViewController {
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier) // here we are registering a cell that has a reuse identifier
         tableView.rowHeight = 64
     }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self // this is a protocol we have to conform to, we have to implement a function that helps us filter through our users in our search controller
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -48,15 +66,43 @@ class SearchController: UITableViewController {
 extension SearchController {
     // defines how many rows the table view has
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return inSearchMode ? filteredUsers.count : users.count
     }
     
     // defines what each cell in the table view contains
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        
+        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
         //print("Func called")
         //print("DEBUG: Index path row is \(indexPath.row)")
-        cell.user = users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: user)
+        
         return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        let controller = ProfileController(user: user)
+        navigationController?.pushViewController(controller, animated: true)
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension SearchController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        
+        // we check if the username or fullname contains the text in the search bar
+        filteredUsers = users.filter({ $0.username.contains(searchText) || $0.fullname.lowercased().contains(searchText)
+        }) // the $0 notation represents each one of the users in the users array (we are saying if any on eof the usernames or fullnames contains that text, then add it to the filteredUsers array)
+        
+        print("DEBUG: Filtered users \(filteredUsers)")
+        self.tableView.reloadData()
     }
 }
