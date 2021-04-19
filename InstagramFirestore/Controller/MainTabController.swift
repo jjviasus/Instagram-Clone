29c42989c2b73659ba34eeb84f547151cc4bdd6c
jjviasus <61569318+jjviasus@users.noreply.github.com>
2021-04-19 19:14:41 -0400

    Image Selector

--- InstagramFirestore/Controller/MainTabController.swift
+++ InstagramFirestore/Controller/MainTabController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import YPImagePicker
 
 // our custom root view controller of our application
 class MainTabController: UITabBarController {
@@ -55,6 +56,7 @@ class MainTabController: UITabBarController {
     // Where we set up all the view controllers for the tab bar controller
     func configureViewControllers(withUser user: User) {
         view.backgroundColor = .white
+        self.delegate = self
         
         let layout = UICollectionViewFlowLayout() // use this to initialize the feed controller
         let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout)) // collectionViewLayout is a parameter of UICollectionViewController
@@ -96,6 +98,16 @@ class MainTabController: UITabBarController {
         // Return our navigation controller
         return nav
     }
+    
+    func didFinishPickingMedia(_ picker: YPImagePicker) {
+        // Next button clicked after selecting image
+        picker.didFinishPicking { items, _ in
+            picker.dismiss(animated: true) {
+                guard let selectedImage = items.singlePhoto?.image else { return }
+                print("DEBUG: Selected image is \(selectedImage)")
+            }
+        }
+    }
 }
 
 // MARK: - AuthenticationDelegate
@@ -104,5 +116,32 @@ extension MainTabController: AuthenticationDelegate {
     func authenticationDidComplete() {
         fetchUser()
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - UITabBarControllerDelegate
+
+extension MainTabController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let index = viewControllers?.firstIndex(of: viewController)
+        
+        if index == 2 {
+            var config = YPImagePickerConfiguration()
+            config.library.mediaType = .photo
+            config.shouldSaveNewPicturesToAlbum = false
+            config.startOnScreen = .library
+            config.screens = [.library]
+            config.hidesStatusBar = false
+            config.hidesBottomBar = false
+            config.library.maxNumberOfItems = 1
+            
+            let picker = YPImagePicker(configuration: config)
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+            
+            didFinishPickingMedia(picker)
+        }
+        
+        return true
     }
 }
