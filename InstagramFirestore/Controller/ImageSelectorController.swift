810e08f5909438659085bd23fc902f4dcc7e3f0f
jjviasus <61569318+jjviasus@users.noreply.github.com>
2021-04-21 00:01:09 -0400

    Upload Post UI

--- InstagramFirestore/Controller/ImageSelectorController.swift
+++ InstagramFirestore/Controller/ImageSelectorController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ImageSelectorController: UIViewController {
     
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .white
     }
 }
---
+++ InstagramFirestore/Utils/InputTextView.swift
@@ -0,0 +1,12 @@
+//
+//  InputTextView.swift
+//  InstagramFirestore
+//
+//  Created by Justin Viasus on 4/20/21.
+//
+
+import UIKit
+
+class InputTextView: UITextView {
+    
+}
